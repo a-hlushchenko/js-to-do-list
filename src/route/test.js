1d428cfa2fb6a0b3eccad6fe075d88caf7e674f6
AntonHlushchenko <glantoshka@gmail.com>
2023-08-31 12:41:33 +0300

    upd title

--- src/route/test.js
+++ src/route/test.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const Test = require('../class/test')
-
 router.get('/', function (req, res) {
   res.render('index', {
     name: 'index',
     component: ['heading'],
 
-    title: 'Назва сторінки',
-    data: {
-      test: new Test().test,
-    },
+    title: 'to-do list',
+    data: {},
   })
 })
 
